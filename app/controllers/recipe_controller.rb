3b3b39388f5839cb70c208267f044d4a63b06d7f
bretburau <github email address>
2018-01-03 07:17:36 -0600

    starting recipe controller

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,3 +1,10 @@
 class RecipeController < ApplicationController
 
+  get '/recipes' do
+    erb :'/recipes/list'
+  end
+
+  get '/recipes/new' do
+    erb :'/recipes/new'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  # has_many :groceries, through :
+  # has_many :groceries, through :recipes  ####todo set this relation up
   has_many :recipes
   has_secure_password
 end
