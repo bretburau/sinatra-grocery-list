6359af104bc3dbfcd8bb1f2950357826c8ecf25a
bretburau <github email address>
2018-01-02 21:19:41 -0600

    added login page/routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -19,4 +19,16 @@ class UserController < ApplicationController
     get '/login' do
       erb :'users/login'
     end
+
+    post '/login' do
+      session[:user_id] = params[:user_id]
+    end
+
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+  
+    def logged_in?
+      !!session[:user_id]
+    end
 end
