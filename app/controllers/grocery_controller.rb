0d0a78e4ab7a5119b66d11f9afb2e61975ba1af0
bretburau <github email address>
2018-01-05 12:52:55 -0600

    working on grocery edit

--- app/controllers/grocery_controller.rb
+++ app/controllers/grocery_controller.rb
@@ -19,4 +19,28 @@ class GroceryController < ApplicationController
       erb :'/groceries/list'
     end
   end
+
+  get '/groceries/:slug' do
+    @grocery = Grocery.find_by_slug(params[:slug])
+    erb :'/groceries/show'
+    ##Show page for slugged ingredient TODO 
+  end
+
+  get '/groceries/:slug/edit' do
+    @grocery = Grocery.find_by_slug(params[:slug])
+    erb :'/groceries/edit'
+    ##show page for patching ingredient via slug TODO
+  end
+
+  patch '/groceries/:slug/edit' do
+    @grocery = Grocery.find_by_slug(params[:slug])
+    @grocery.name = params[:name] if !params[:name].empty?
+    @grocery.price = params[:price] if !params[:price].empty?
+    flash[:message] = "Grocery updated"
+    erb :'/groceries/#{@grocery.slug}'
+  end
+
+  post '/groceries/:slug/delete' do
+    ###delete grocery
+  end
 end
--- app/models/grocery.rb
+++ app/models/grocery.rb
@@ -1,3 +1,11 @@
 class Grocery < ActiveRecord::Base
   belongs_to :recipe
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.select {|name| name.slug == slug}.last      
+  end
 end
