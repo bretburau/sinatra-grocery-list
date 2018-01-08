e3f83cdd9a61e8ba3baf0eeb9114b96b4b94a608
bretburau <github email address>
2018-01-08 13:05:25 -0600

    Working on new recipe controller

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -7,4 +7,14 @@ class RecipeController < ApplicationController
   get '/recipes/new' do
     erb :'/recipes/new'
   end
+
+  post '/recipes/new' do
+    if params[:recipe][:name].empty?
+      flash[:message] = "Please enter a recipe name"
+      erb :'/recipes/new'
+    else
+      binding.pry
+      recipe = Recipe.new(params)
+    end
+  end
 end
